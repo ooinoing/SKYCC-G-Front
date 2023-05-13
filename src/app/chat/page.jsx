53b21bc2576e8aa45a06183426a99635cafee806
jeeho <ooinoing@gmail.com>
2023-05-13 22:48:46 +0900

    first commit

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -3,8 +3,12 @@ import ChatBox from "@/components/ChatBox";
 export default function Chat() {
   return (
     <div id="chatbot-container">
-      <div id="chatbot-messages">
-        <ChatBox />
+      <div className="w-full pt-5">
+        <ChatBox isMe={true}>아니</ChatBox>
+        <ChatBox isMe={false}>뭐가</ChatBox>
+        <ChatBox isMe={true}>연락 좀 빨리 보라고</ChatBox>
+        <ChatBox isMe={true}>자료 조사 어디까지 함?</ChatBox>
+        <ChatBox isMe={true}>나 저녁 약속이라 빨리 해야됨</ChatBox>
       </div>
       <div className="absolute bottom-0" id="user-input">
         <input type="text" id="user-message" placeholder="Type your message..." />
--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,3 +1,14 @@
 export default function ChatBox() {
   return <div>This is Chat Box</div>;
 }
+export default function ChatBox(props) {
+  const { isMe, children } = props;
+  const boxColor = isMe ? "bg-yellow-400" : "bg-white";
+  const boxPosition = isMe ? "justify-end" : "justify-start";
+
+  return (
+    <div className={`flex ${boxPosition} w-full px-5`}>
+      <div className={`${boxColor}   rounded-xl w-fit px-4 py-1 mb-2 `}>{children}</div>
+    </div>
+  );
+}
